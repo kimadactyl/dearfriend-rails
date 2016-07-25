64462696649392e33592f15e33340d8dffc2a0c4
Kim Foale <kim@alliscalm.net>
2016-07-25 17:57:22 +0100

    Multiple view migrations

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :recipients
   resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/contact", to: "static_pages#contact"
+  get "/contribute", to: "static_pages#contribute"
   root to: "static_pages#index"
 end
