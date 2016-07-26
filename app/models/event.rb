b3685734042bb729007d465ee73aadaae045f7cf
Kim Foale <kim@alliscalm.net>
2016-07-26 17:13:17 +0100

    Improve events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,17 @@
 class Event < ApplicationRecord
   validates_presence_of :start, :finish, :title, :description
+
+  def address
+    [self.street1, self.street2, self.city, self.postcode].reject{ |v| v.empty? }.join(", ")
+  end
+
+  def from_until
+    full_date = "%B %e, %I:%M %p"
+    time_only = "%I:%M %p"
+    if self.start.to_date == self.finish.to_date
+      self.start.strftime(full_date) + " — " + self.finish.strftime(time_only)
+    else
+      self.start.strftime(full_date) + " — " + self.finish.strftime(full_date)
+    end
+  end
 end
