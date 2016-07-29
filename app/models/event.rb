d09f8c438aa7076311bbf9b8d1b080f834db12e4
Kim Foale <kim@alliscalm.net>
2016-07-29 15:04:25 +0100

    Fix and strip tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,15 +2,17 @@ class Event < ApplicationRecord
   validates_presence_of :start, :finish, :title, :description
 
   def address
-    [self.street1, self.street2, self.city, self.postcode].reject{ |v| v.empty? }.join(", ")
+    if self.street1 || self.street2 || self.city || self.postcode
+      [self.street1, self.street2, self.city, self.postcode].reject{ |v| v.empty? }.join(", ")
+    end
   end
 
   def from_until
-
     if self.start.to_date == self.finish.to_date
       self.start.strftime(FULL_DATE) + " — " + self.finish.strftime(TIME_ONLY)
     else
       self.start.strftime(FULL_DATE) + " — " + self.finish.strftime(FULL_DATE)
     end
   end
+
 end
