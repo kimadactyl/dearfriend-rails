97003bef829462a3f9745c57a99c1afb18f7ecea
Kim Foale <kim@alliscalm.net>
2017-03-05 15:11:53 +0000

    Fix routes and remove more devise gunk

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  # Clearance
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  # Administrate
   namespace :admin do
     resources :authors
     resources :events
@@ -7,10 +21,12 @@ Rails.application.routes.draw do
     resources :letter_authors
     resources :letter_recipients
     resources :recipients
+    resources :users
 
     root to: "letters#index"
   end
 
+  # App itself
   resources :events, only: [:index]
   resources :letter_recipients, only: [:index, :show]
   resources :letter_authors, only: [:index, :show]
